7093b7e30e23506d860f540ba87b918506edf575
myyrakle <sssang97@naver.com>
2019-04-10 11:36:53 +0900

    Create gcc.c

---
+++ mains/gcc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/*gcc only*/
+/*C11*/
+__attribute__ ((cdecl))
+__attribute__ ((deprecated))
+__attribute__ ((noinline))
+__attribute__ ((nothrow))
+__attribute__ ((pure))
+__attribute__ ((const))
+_Noreturn
+_Atomic 
+const volatile auto typeof((int32_t)0ULL) 
+main
+(_Atomic const register volatile int64_t argc, 
+_Atomic volatile register const int8_t* restrict
+const restrict * const restrict args, ...)
+{
+	puts("hello");
+}
