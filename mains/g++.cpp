fbf402189e0e9f2a4deef4ca7cb4653a9d0f923e
myyrakle <sssang97@naver.com>
2019-04-10 11:35:42 +0900

    Create g++.cpp

---
+++ mains/g++.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+[[deprecated]]
+[[noreturn]] 
+[[carries_dependency]]
+alignas(decltype(getchar()))
+__attribute__ ((cdecl)) /*gcc only*/
+__attribute__ ((always_inline))
+extern volatile 
+const auto main(
+	register volatile const int 
+	argument_count = NULL,
+	register volatile 
+	decltype(std::string().c_str()) 
+	arguments[] = {nullptr,}) noexcept
+->decltype(std::vector
+	<decltype(decltype(int{-1}){155}%3)>
+	::value_type{-33})
+__attribute__ ((const))
+__attribute__ ((externally_visible))
+__attribute__ ((nothrow))
+{
+	std::cout<<"hello";
+}
